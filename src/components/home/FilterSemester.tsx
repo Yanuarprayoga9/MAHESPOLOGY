c077e93d2ad1a45b7d88f7218cafdc8ba7c8f60f
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-07-04 21:22:59 +0700

    UPDATE semester content

--- src/components/home/FilterSemester.tsx
+++ src/components/home/FilterSemester.tsx
@@ -3,12 +3,12 @@ import { motion } from 'framer-motion';
 
 const FilterSemester = () => {
   return (
-    <div className="  h-auto px-5 lg:px-[72px] flex flex-col gap-5 mt-4">
+    <div className=" py-8 h-auto px-5 lg:px-[72px] flex flex-col gap-5 mt-4">
       <h1 className="font-bold text-[16px] lg:text-[20px] text-orange">
         Halo ngab
       </h1>
       <h1 className="font-bold text-2xl lg:text-[30px]">
-        Jadi ada 3 tipe mahasiswa nih,lo yang mana
+        Jadi ada 3 fase mahasiswa nih,lo yang mana
       </h1>
       <div className="flex  flex-wrap justify-center">
         {features.map((item, i) => {
@@ -18,31 +18,29 @@ const FilterSemester = () => {
               initial={{ opacity: 0, scale: 0 }}
               whileInView={{ opacity: 1, scale: 1 }}
               transition={{ duration: 0.1 }}
+              whileHover={{ 
+                scale:1.1,
+                boxShadow: '0px 0px 10px 0px rgba(255,255,255,1)' // Bayangan putih
+              }}
               // viewport={{ once: true }}
+              className='m-6 py-8'
             >
               <div
-                className="flex flex-col gap-3 justify-center items-center  "
+                className="flex flex-col p-4 h-[300px]  justify-center items-center shadow-xl  shadow-purple-950"
                 key={i}
               >
-                <motion.div
-                  
-                  whileHover={{ 
-                    scale:1.3
-                   }}
-                  // viewport={{ once: true }}
-                >
-                  <div className="w-[300px] h-[300px] flex justify-center">
+          
+                  <div className="w-[200px] h-[200px] flex justify-center">
                     <img
                       src={item.image}
                       alt=""
-                      className="w-[200px] h-full object-contain"
+                      className="w-[200px] h-[200px] object-contain"
                     />
                   </div>
-                </motion.div>
                 <h1 className="text-[16px] lg:text-[20px] font-bold">
                   {item.title}
                 </h1>
-                <p className="text-center text-text text-[16px] lg:text-[20px] max-w-[400px]">
+                <p className="text-center  text-text text-sm lg:text-[20px] max-w-[400px]">
                   {item.desc}
                 </p>
               </div>

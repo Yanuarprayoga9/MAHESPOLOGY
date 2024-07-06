b5cbca4a8f6d83f3ecb1e4d6d732375511be010b
Yanuarprayoga9 <yanuarprayoga.stu@pnc.ac.id>
2024-07-06 18:07:55 +0700

    UPDATE img filter semester

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,3 +1,3 @@
 export { default as MINI1 } from '../assets/Group.png';
-export { default as MINI3 } from '../asset/MINI3.svg';
-export { default as MINI2 } from '../asset/MINI2.svg';
+export { default as MINI3 } from '../assets/Gaming-On-Portable-Consoles-1--Streamline-Manila.png';
+export { default as MINI2 } from '../assets/Modular-Coding-Of-Application--Streamline-Manila.png';
